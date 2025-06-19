9d10d5e4cdedb8072cdb78d130b88f84aac3587e
romerocts <04.romeromanuel@gmail.com>
2025-06-19 15:21:49 -0500

    combios esteticos

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Ferretería Araque - Sistema de Gestión",
+  title: "Gestor - Sistema de Gestión",
   description: "Sistema de gestión para ferretería con inventario y préstamos",
   generator: 'by romerocts'
 }
